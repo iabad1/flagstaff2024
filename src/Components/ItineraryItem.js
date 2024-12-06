062586cdbff4629023bfaae9783c6e78485b753d
iabadaa <63217881+iabad1@users.noreply.github.com>
2024-12-06 13:27:07 -0700

    rerun

--- src/Components/ItineraryItem.js
+++ src/Components/ItineraryItem.js
@@ -18,7 +18,7 @@ export default function ItineraryItem({itineraryItem}){
                             }
                                 
                                 
-                            </span> : ""
+                            </span> : " "
                         } 
                        
                     </td>
